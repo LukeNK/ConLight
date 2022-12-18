089d4724b3dffc718d9b3a0681290b4de69f1b92
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2022-12-17 22:31:40 -0800

    Ellipse draw function Line use min max instead of canvas.width

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ canvas.h = canvas.width / 2; // canvas shift
 canvas.k = canvas.height / 2; 
 const ctx = canvas.getContext('2d');
 
-// In all calculation, it pretends as if we are doing it at the origin and stuff. When drawing, just simply shift the graph to the correct position and flip it
+// In all calculation, it pretends as if we are doing it at the origin
+// When drawing, just simply shift the graph to the correct position
 class Graph {
     /**
      * Create a graph will all necessary data to draw it (if needed)
@@ -18,25 +19,31 @@ class Graph {
         switch (type) {
             case 'ellipse':
                 this.a = coe.a; this.b = coe.b;
-                this.h = coe.h; this.k = coe.k;
+                this.h = coe.h || 0; this.k = coe.k || 0;
                 break;
             case 'line':
             default:
                 this.m = coe.m;
-                this.k = coe.k;
+                this.k = coe.k || 0;
                 break;
         }
         // set min and max to out of view for ease of calculation
         this.minX = minX || -canvas.width;
         this.maxX = maxX || canvas.width;
     }
+    /**
+     * Calculate x to get y
+     * @param {Number} x Input for the function/equation 
+     * @returns {(Number|Number[])} In case of function, return a number, else return an array of result
+     */
     calcFunc(x) {
-        // calculate function
         switch (this.type) {
+            case 'ellipse':
+                let inside = (a*a - x*x) / a*a * b*b
+                return [+Math.sqrt(inside), -Math.sqrt(inside)]
             case 'line':
             default:
                 return this.m*x-this.k;
-                break;
         }
     }
     tangent(x, y) {
@@ -44,23 +51,26 @@ class Graph {
         if (this.type != 'ellipse') 
             throw TypeError('Expected invoke on ellipse, got ' + this.type);
         // calculate the tangent slop thanks to Leo
-        let m = (b*b / a*a)*(x, y);
-        let c = y-m*x;
+        let m = (b*b / a*a)*(x, y),
+            c = y-m*x;
         return new Graph('line', {m: m, c: c})
     }
     draw() {
         switch (this.type) {
             case 'ellipse':
-                
+                ctx.beginPath()
+                ctx.ellipse(this.h + canvas.h, this.k + canvas.k, this.a, this.b, 0, 0, Math.PI * 2)
+                ctx.stroke()
                 break;
-            
             case 'line':
             default:
                 let k = -this.m * canvas.h + this.k + canvas.k;
                 ctx.beginPath();
-                ctx.moveTo(0, k)
-                ctx.lineTo(canvas.width, this.m * canvas.width + k);
-                ctx.stroke()
+                let minX = this.minX + canvas.h,
+                    maxX = this.maxX + canvas.h;
+                ctx.moveTo(minX, this.m * minX + k);
+                ctx.lineTo(maxX, this.m * maxX + k);
+                ctx.stroke();
                 break;
         }
     }
